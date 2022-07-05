5933b5958d3104be0ef0ac2252238000e092850f
mayragann <mayragann@gmail.com>
2022-07-05 11:48:52 -0400

    Added Functionality to game

--- rpsls/src/App.js
+++ rpsls/src/App.js
@@ -1,24 +1,66 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [userChoice, setUserChoice] = useState(null)
   const [computerChoice, setComputerChoice] = useState(null)
+  const [result, setResult] = useState(null)
   const choices = ['rock', 'paper' ,'scissors', 'lizard' , 'spock']
+  
   const  handleClick = (value) => {
     setUserChoice(value)
+    generateComputerChoice()
   }
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(randomChoice)
+  }
+  useEffect(() => {
+    checkResult()
+    switch( userChoice + computerChoice) {
+      case 'scissorspaper':
+      case 'rockscissors' :
+      case 'paperrock':
+      case 'rocklizard':
+      case 'lizardspock':
+      case 'spockscissors':
+      case 'scissorslizard':
+      case 'lizardpaper':
+      case 'paperspock':
+      case 'spockrock':
+        setResult('YOU WIN!!')
+        break
+      case 'paperscissors':
+      case 'scissorsrock':
+      case 'rockpaper':
+      case 'lizardrock':
+      case 'spocklizard':
+      case 'scissorsspock':
+      case 'lizardsscissors':
+      case 'paperlizard':
+      case 'spockpaper':
+      case 'rockspock':
+        setResult('YOU LOSE!!')
+        break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+        case 'lizardlizard':
+        case 'spockspock':
+          setResult('IT IS A DRAW')
+          break
+    }
+  }, [computerChoice, userChoice])
 
+  const checkResult = () => {
+   
+  }
 
   return (
     <div >
       <h1> user Choice is: {userChoice} </h1>
-      <h1>computer Choice is:</h1>
-      {/* <button onClick={ () => handleClick('rock')}>Rock</button>
-      <button onClick={ () => handleClick('paper')}>Paper</button>
-      <button onClick={ () => handleClick('scissors')}>Scissors</button>
-      <button onClick={ () => handleClick('lizard')}>Lizard</button>
-      <button onClick={ () => handleClick('spock')}>Spock</button> */}
-      {choices.map(choice => <button onClick={ () => handleClick(choice)}>{choice}</button>)}
+      <h1>computer Choice is: {computerChoice}</h1>
+      {choices.map((choice) => <button key={choice} onClick={ () => handleClick(choice)}>{choice}</button>)}
+      <h1>{result}</h1>
     </div>
   );
 }

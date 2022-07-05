4c7bb87f431c6d9680a3109c03b754db2848615e
mayragann <mayragann@gmail.com>
2022-07-05 14:51:23 -0400

    Added Full game in App

--- rpsls/src/App.js
+++ rpsls/src/App.js
@@ -1,66 +1,122 @@
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { FaHandRock, FaHandPaper, FaHandScissors, FaHandLizard, FaHandSpock } from "react-icons/fa";
+import { useState } from "react";
+
 
 function App() {
-  const [userChoice, setUserChoice] = useState(null)
-  const [computerChoice, setComputerChoice] = useState(null)
-  const [result, setResult] = useState(null)
-  const choices = ['rock', 'paper' ,'scissors', 'lizard' , 'spock']
+  const actions = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"],
+  };
   
-  const  handleClick = (value) => {
-    setUserChoice(value)
-    generateComputerChoice()
+  function randomAction() {
+    const keys = Object.keys(actions);
+    const index = Math.floor(Math.random() * keys.length);
+  
+    return keys[index];
   }
-  const generateComputerChoice = () => {
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
-    setComputerChoice(randomChoice)
-  }
-  useEffect(() => {
-    checkResult()
-    switch( userChoice + computerChoice) {
-      case 'scissorspaper':
-      case 'rockscissors' :
-      case 'paperrock':
-      case 'rocklizard':
-      case 'lizardspock':
-      case 'spockscissors':
-      case 'scissorslizard':
-      case 'lizardpaper':
-      case 'paperspock':
-      case 'spockrock':
-        setResult('YOU WIN!!')
-        break
-      case 'paperscissors':
-      case 'scissorsrock':
-      case 'rockpaper':
-      case 'lizardrock':
-      case 'spocklizard':
-      case 'scissorsspock':
-      case 'lizardsscissors':
-      case 'paperlizard':
-      case 'spockpaper':
-      case 'rockspock':
-        setResult('YOU LOSE!!')
-        break
-        case 'paperpaper':
-        case 'scissorsscissors':
-        case 'rockrock':
-        case 'lizardlizard':
-        case 'spockspock':
-          setResult('IT IS A DRAW')
-          break
+  
+  function calculateWinner(action1, action2) {
+    if (action1 === action2) {
+      return 0;
+    } else if (actions[action1].includes(action2)) {
+      return -1;
+    } else if (actions[action2].includes(action1)) {
+      return 1;
     }
-  }, [computerChoice, userChoice])
-
-  const checkResult = () => {
-   
+  
+    // This should never really happen
+    return null;
   }
+  
+  function ActionIcon({ action, ...props }) {
+    const icons = {
+      rock: FaHandRock,
+      paper: FaHandPaper,
+      scissors: FaHandScissors,
+      lizard: FaHandLizard,
+      spock: FaHandSpock,
+    };
+    const Icon = icons[action];
+    return <Icon {...props} />;
+  }
+  
+  function Player({ name = "Player", score = 0, action = "rock" }) {
+    return (
+      <div className="player">
+        <div className="score">{`${name}: ${score}`}</div>
+        <div className="action">
+          {action && <ActionIcon action={action} size={60} />}
+        </div>
+      </div>
+    );
+  }
+  
+  function ActionButton({ action = "rock", onActionSelected }) {
+    return (
+      <button className="round-btn" onClick={() => onActionSelected(action)}>
+        <ActionIcon action={action} size={20} />
+      </button>
+    );
+  }
+  
+  function ShowWinner({winner = 0}) {
+    const text = {
+      "-1": "You Win!",
+      0: "It's a Tie",
+      1: "You Lose!",
+    };
+  
+    return (
+      <h2>{text[winner]}</h2>
+    )
+  }
+  const [playerAction, setPlayerAction] = useState("");
+  const [computerAction, setComputerAction] = useState("");
+
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [winner, setWinner] = useState(0);
+
+  const onActionSelected = (selectedAction) => {
+    const newComputerAction = randomAction();
+
+    setPlayerAction(selectedAction);
+    setComputerAction(newComputerAction);
+
+    const newWinner = calculateWinner(selectedAction, newComputerAction);
+    setWinner(newWinner);
+    if (newWinner === -1) {
+      setPlayerScore(playerScore + 1);
+    } else if (newWinner === 1) {
+      setComputerScore(computerScore + 1);
+    }
+  };
 
   return (
-    <div >
-      <h1> user Choice is: {userChoice} </h1>
-      <h1>computer Choice is: {computerChoice}</h1>
-      {choices.map((choice) => <button key={choice} onClick={ () => handleClick(choice)}>{choice}</button>)}
-      <h1>{result}</h1>
+    <div className="center">
+      <h1>Rock Paper Scissors</h1>
+      <div>
+        <div className="container">
+          <Player name="Player" score={playerScore} action={playerAction} />
+          <Player
+            name="Computer"
+            score={computerScore}
+            action={computerAction}
+          />
+        </div>
+        <div>
+          <ActionButton action="rock" onActionSelected={onActionSelected} />
+          <ActionButton action="paper" onActionSelected={onActionSelected} />
+          <ActionButton action="scissors" onActionSelected={onActionSelected} />
+          <ActionButton action="lizard" onActionSelected={onActionSelected} />
+          <ActionButton action="spock" onActionSelected={onActionSelected} />
+        </div>
+        <ShowWinner winner={winner}/>
+      </div>
     </div>
   );
 }
